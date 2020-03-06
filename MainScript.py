7d682e842924a4ebc94c0fe5d848d19e80a87b28
tel3port <carn3lian@gmail.com>
2020-03-06 17:03:50 +0300

    trying to adjust window size to get the bot to work

--- MainScript.py
+++ MainScript.py
@@ -35,12 +35,11 @@ class MastodonBot:
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         chrome_options.add_argument("--disable-dev-sgm-usage")
         chrome_options.add_argument("--no-sandbox")
-        # chrome_options.add_argument("--start-maximized")
-        # prefs = {"profile.managed_default_content_settings.images": 2}
-        # chrome_options.add_experimental_option("prefs", prefs)
-        chrome_options.add_argument("--headless")
-        self.driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-        # self.driver = webdriver.Chrome("./chromedriver", options=chrome_options)
+        chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument('window-size=2560,1440')
+        # chrome_options.add_argument("--headless")
+        # self.driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+        self.driver = webdriver.Chrome("./chromedriver", options=chrome_options)
         self. base_url = "https://mastodon.social/"
         self.login()
 
@@ -62,6 +61,7 @@ class MastodonBot:
         return response_list[randint(0, len(response_list) - 1)]
 
     def login(self):
+        self.driver.maximize_window()
         print("session id at login: ", self.driver.session_id)
 
         login_btn_xpath = "//button[contains(.,'Log in')]"
